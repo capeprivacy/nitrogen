8c3d9114e3b1db8f1c282ddd236768cf05e5cdc1
jvmncs <jason@manc.us>
2022-10-18 16:56:33 -0400

    a bit more on the cli structure

--- src/main.rs
+++ src/main.rs
@@ -11,18 +11,30 @@ struct Cli {
 enum Commands {
     /// Provision nitro-enabled ec2 instances
     Launch {
-        name: String,
+        /// Name of the provisioned EC2 instance
+        #[arg(short,long)]
+        instance_name: String,
         /// EC2-instance type. Must be Nitro compatible.
+        #[arg(short,long)]
         instance_type: String,
         /// EC2-instance port for socat enclave connection
-        port: usize,
+        #[arg(short,long)]
+        socat_port: usize,
+        /// SSH key-pair
+        #[arg(short,long)]
+        key_name: String,
+        /// IP address range that can be used to SSH to the EC2 instance. Defaults to anywhere ("0.0.0.0/0").
+        #[arg(short,long)]
+        ssh_location: Option<String>,
     },
 
     /// Build a Nitro EIF from a given Dockerfile
     Build {
         // Dockerfile location
+        #[arg(short,long)]
         dockerfile: String,
         // Output EIF location
+        #[arg(short,long)]
         eif: String,
     },
 
@@ -38,11 +50,12 @@ enum Commands {
 fn main() {
     let cli = Cli::parse();
     match cli.command {
-        Commands::Launch{name, instance_type, port} => {
+        Commands::Launch{name, instance_type, port, ssh_location} => {
             println!("Launching instance...");
             println!("Name: {}", name);
             println!("Instance type: {}", instance_type);
             println!("Port: {}", port);
+            let _ssh_location = ssh_location.unwrap_or("0.0.0.0/0");
             todo!("implement launch command logic");
         }
         Commands::Build{..} => {
