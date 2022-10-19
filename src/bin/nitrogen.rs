de3f0a924c714f31bc38949202a310b68a85f67e
Justin Patriquin <justinpatriquin@gmail.com>
2022-10-19 14:22:53 -0300

    build

--- src/bin/nitrogen.rs
+++ src/bin/nitrogen.rs
@@ -1,9 +1,12 @@
-use std::path::Path;
 use failure::Error;
+use std::path::Path;
 
 use aws_sdk_cloudformation::{Client, Region};
 use clap::{Parser, Subcommand};
+use home;
+use nitro_cli::build_from_docker;
 use tokio::fs;
+use tokio::process::Command;
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -23,10 +26,10 @@ enum Commands {
         #[arg(long)]
         instance_type: String,
         /// EC2-instance port for socat enclave connection
-        #[arg(short,long, default_value_t=5000)]
+        #[arg(short, long, default_value_t = 5000)]
         port: usize,
         /// SSH key-pair
-        #[arg(short,long)]
+        #[arg(short, long)]
         key_name: String,
         /// IP address range that can be used to SSH to the EC2 instance. Defaults to anywhere ("0.0.0.0/0").
         #[arg(short, long)]
@@ -36,10 +39,10 @@ enum Commands {
     /// Build a Nitro EIF from a given Dockerfile
     Build {
         // Dockerfile location
-        #[arg(short,long)]
+        #[arg(short, long)]
         dockerfile: String,
         // Output EIF location
-        #[arg(short,long)]
+        #[arg(short, long)]
         eif: String,
     },
 
@@ -64,7 +67,13 @@ async fn main() -> Result<(), Error> {
     let shared_config = aws_config::from_env();
 
     match cli.command {
-        Commands::Launch{instance_name, instance_type, port, key_name, ssh_location} => {
+        Commands::Launch {
+            instance_name,
+            instance_type,
+            port,
+            key_name,
+            ssh_location,
+        } => {
             println!("Launching instance...");
             println!("Instance Name: {}", instance_name);
             println!("Instance type: {}", instance_type);
@@ -72,13 +81,41 @@ async fn main() -> Result<(), Error> {
             println!("Key Name: {}", key_name);
             let _ssh_location = ssh_location.unwrap_or("0.0.0.0/0".to_string());
             todo!("implement launch command logic");
-            // TODO bundle this template into binary w/ `build.rs`
-            let launch_template = fs::read_to_string(Path::new("src/templates/launchTemplate.json")).await?;
+            // TODO bundle this template into binary w/ `build.rs`et cmd =
+            let launch_template =
+                fs::read_to_string(Path::new("src/templates/launchTemplate.json")).await?;
         }
-        Commands::Build{..} => {
-            todo!("implement build command logic");
+        Commands::Build { dockerfile, .. } => {
+            Command::new("docker")
+                .args(["build", "-t", "nitrogen-build", ".", "-f", &dockerfile])
+                .output()
+                .await
+                .expect("failed to build docker image");
+
+            // docker run -v ~/.docker:/root/.docker -v /var/run/docker.sock:/var/run/docker.sock capeprivacy/eif-builder build-enclave --docker-uri docker.io/capeprivacy/runtime:release-ffc4e1c --output-file runtime.eif
+
+            let h = home::home_dir().unwrap();
+            let out = Command::new("docker")
+                .args([
+                    "run",
+                    "-v",
+                    &format!("{}/.docker:/root/.docker", h.display()),
+                    "-v",
+                    "/var/run/docker.sock:/var/run/docker.sock",
+                    "capeprivacy/eif-builder:latest",
+                    "build-enclave",
+                    "--docker-uri",
+                    "nitrogen-build",
+                    "--output-file",
+                    "runtime.eif",
+                ])
+                .output()
+                .await
+                .unwrap();
+            println!("{:?}", out);
+            Ok(())
         }
-        Commands::Deploy{..} => {
+        Commands::Deploy { .. } => {
             todo!("implement deploy command logic");
         }
         Commands::Delete{..} => {
