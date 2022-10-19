b0f3727c4f0881b97de76979f9d7a4a411ba1f60
jvmncs <jason@manc.us>
2022-10-19 11:04:37 -0400

    pass build, clean up some cli args, add deps we'll need

--- src/bin/nitrogen.rs
+++ src/bin/nitrogen.rs
@@ -1,4 +1,9 @@
+use std::path::Path;
+use failure::Error;
+
+use aws_sdk_cloudformation::{Client, Region};
 use clap::{Parser, Subcommand};
+use tokio::fs;
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -12,19 +17,19 @@ enum Commands {
     /// Provision nitro-enabled ec2 instances
     Launch {
         /// Name of the provisioned EC2 instance
-        #[arg(short,long)]
+        #[arg(long)]
         instance_name: String,
         /// EC2-instance type. Must be Nitro compatible.
-        #[arg(short,long)]
+        #[arg(long)]
         instance_type: String,
         /// EC2-instance port for socat enclave connection
-        #[arg(short,long)]
-        socat_port: usize,
+        #[arg(short,long, default_value_t=5000)]
+        port: usize,
         /// SSH key-pair
         #[arg(short,long)]
         key_name: String,
         /// IP address range that can be used to SSH to the EC2 instance. Defaults to anywhere ("0.0.0.0/0").
-        #[arg(short,long)]
+        #[arg(short, long)]
         ssh_location: Option<String>,
     },
 
@@ -47,16 +52,22 @@ enum Commands {
     },
 }
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), Error> {
     let cli = Cli::parse();
+    let shared_config = aws_config::from_env();
+
     match cli.command {
-        Commands::Launch{name, instance_type, port, ssh_location} => {
+        Commands::Launch{instance_name, instance_type, port, key_name, ssh_location} => {
             println!("Launching instance...");
-            println!("Name: {}", name);
+            println!("Instance Name: {}", instance_name);
             println!("Instance type: {}", instance_type);
-            println!("Port: {}", port);
-            let _ssh_location = ssh_location.unwrap_or("0.0.0.0/0");
+            println!("Socat Port: {}", port);
+            println!("Key Name: {}", key_name);
+            let _ssh_location = ssh_location.unwrap_or("0.0.0.0/0".to_string());
             todo!("implement launch command logic");
+            // TODO bundle this template into binary w/ `build.rs`
+            let launch_template = fs::read_to_string(Path::new("src/templates/launchTemplate.json")).await?;
         }
         Commands::Build{..} => {
             todo!("implement build command logic");
