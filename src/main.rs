9d3392daf0bc08a1a707c4d31bfbea585d4adf79
Jenny <jenny@capeprivacy.com>
2022-10-19 14:29:40 -0400

    add deploy, not tested yet

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use clap::{Parser, Subcommand};
+use std::process::Command;
+
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -28,10 +30,16 @@ enum Commands {
 
     /// Deploy an EIF to a provisioned nitro ec2 instance
     Deploy {
-        /// Name of the provisioned instance
+        /// Domain of the provisioned ec2 instance
         instance: String,
-        // Filepath to EIF
+        /// Filepath to EIF
         eif: String,
+        /// Filepath to SSH key for the instance
+        ssh_key: String,
+        /// Number of CPUs to provision for the enclave
+        cpu_count: String,
+        /// Memory in MB to provision for the enclave
+        memory: String,
     },
 }
 
@@ -48,8 +56,19 @@ fn main() {
         Commands::Build{..} => {
             todo!("implement build command logic");
         }
-        Commands::Deploy{..} => {
-            todo!("implement deploy command logic");
+        Commands::Deploy{instance, eif, ssh_key, cpu_count, memory} => {
+            loop {
+                let _result = Command::new("ssh")
+                    .args(["-i", &ssh_key, &("ec2-user@".to_owned()+&instance)])
+                    .args(["nitro-cli", "run-enclave"])
+                    .args(["--encalve-cid", "16"])
+                    .args(["--eif-path", &eif, "--cpu-count", &cpu_count.to_string()])
+                    .args(["--memory", &memory.to_string()])
+                    .output()
+                    .expect("command failed");
+                // io::stdout().write_all(&result.stdout).unwrap();
+                // io::stderr().write_all(&result.stderr).unwrap();
+            }
         }
     }
 }
