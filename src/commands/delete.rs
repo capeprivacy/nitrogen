15fcc0381867522da11959a2f620200c3d5dd579
jvmncs <jason@capeprivacy.com>
2022-10-27 11:42:15 -0400

    use tokio tracing for progress reporting (#28)

--- src/commands/delete.rs
+++ src/commands/delete.rs
@@ -2,17 +2,14 @@ use crate::commands::setup::check_stack_status;
 use crate::commands::setup::get_stack;
 use aws_sdk_cloudformation::{model::StackStatus, Client};
 use failure::Error;
+use tracing::{info, instrument};
 
 async fn delete_stack(client: &Client, name: &String) -> Result<(), Error> {
-    // TODO tokio tracing, consider instrument
-    println!("Deleting instance...");
-    println!("Instance Name: {}", name);
-
     client.delete_stack().stack_name(name).send().await?;
-
     Ok(())
 }
 
+#[instrument(level = "debug", skip(client))]
 pub async fn delete(client: &Client, name: &String) -> Result<(), Error> {
     let this_stack = get_stack(client, name).await?;
     let stack_id = this_stack.stack_id().unwrap_or_default();
@@ -21,14 +18,14 @@ pub async fn delete(client: &Client, name: &String) -> Result<(), Error> {
 
     let (stack_status, stack_status_reason) = loop {
         let (status, status_reason) = check_stack_status(client, stack_id).await?;
-        tokio::time::sleep(tokio::time::Duration::new(2, 0)).await;
+        tokio::time::sleep(tokio::time::Duration::new(4, 0)).await;
         if status != StackStatus::DeleteInProgress {
             break (status, status_reason);
         }
     };
     match stack_status {
         StackStatus::DeleteComplete => {
-            println!("Successfully deleted stack {:?}", name);
+            info!("Successfully deleted stack '{}'.", name);
         }
         StackStatus::DeleteFailed => {
             return Err(failure::err_msg(
